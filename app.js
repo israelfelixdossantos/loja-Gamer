521f0544c735fe0791efe69c4512a0f8d7f44756
Israel Felix <grrifs@gmail.com>
2025-02-13 10:04:32 -0300

    Em progresso

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //Carregando módulos
 const express = require("express");
-const handlebars = require("express-handlebars");
+const { engine } = require("express-handlebars");
 const bodyParser = require("body-parser");
 const app = express();
+const admin = require("./routes/admin");
+const path = require("path");
 //const mongoose = require("mongoose");
 
 // Configuração
@@ -11,13 +13,34 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //Handlebars
-app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.engine(
+  "handlebars",
+  engine({
+    defaultLayout: "main",
+    layoutsDir: path.join(__dirname, "views/layouts"),
+    partialsDir: path.join(__dirname, "views/partials"),
+  })
+);
+
 app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
+
+// Public
+app.use(express.static(path.join(__dirname, "public")));
 
 // Rotas
+app.get("/", (req, res) => {
+  res.send("Pagina inicial");
+});
+
+app.get("/produtos", (req, res) => {
+  res.send("Lista de produtos");
+});
+
+app.use("/admin", admin);
 
 // Outros
-const PORT = 8081;
+const PORT = 8089;
 app.listen(PORT, () => {
-  console.log("Servidor rodando, localhost:8081");
+  console.log("Servidor rodando, http://localhost:8089");
 });
--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.render("admin/index");
+});
+
+router.get("/produtos", (req, res) => {
+  res.send("Paginas de produtos");
+});
+
+router.get("/categoria", (req, res) => {
+  res.send("Paginas de categorias");
+});
 module.exports = router;
