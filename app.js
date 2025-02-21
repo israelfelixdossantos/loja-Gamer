2bb84c7b2a08f2c756c745555c893bbe4479823e
Israel Felix <grrifs@gmail.com>
2025-02-21 17:18:34 -0300

    finalizado fomulario de cadastro, edição e exclusão de categorias

--- app.js
+++ app.js
@@ -6,8 +6,25 @@ const app = express();
 const admin = require("./routes/admin");
 const path = require("path");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 // Configuração
+// Sessão
+app.use(
+  session({
+    secret: "lojagamer",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
+// Middleware
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  next();
+});
 //Body-parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -28,7 +45,10 @@ app.set("views", path.join(__dirname, "views"));
 // Mongoose
 mongoose.Promise = global.Promise;
 mongoose
-  .connect("mongodb://localhost/Loja")
+  .connect("mongodb://localhost/Loja", {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Conectado ao Mongo");
   })
--- routes/admin.js
+++ routes/admin.js
@@ -13,7 +13,15 @@ router.get("/produtos", (req, res) => {
 });
 
 router.get("/categorias", (req, res) => {
-  res.render("admin/categorias");
+  Categoria.find()
+    .lean()
+    .then((categorias) => {
+      res.render("admin/categorias", { categorias: categorias });
+    })
+    .catch((err) => {
+      req.flash("error_msg", "Houve um erro ao listar as categorias");
+      res.redirect("/admin");
+    });
 });
 
 router.get("/categorias/add", (req, res) => {
@@ -21,18 +29,131 @@ router.get("/categorias/add", (req, res) => {
 });
 
 router.post("/categorias/nova", (req, res) => {
-  const novaCategoria = {
-    nome: req.body.nome,
-    slug: req.body.slug,
-  };
+  var erros = [];
 
-  new Categoria(novaCategoria)
-    .save()
-    .then(() => {
-      console.log("Categoria salva com sucesso!");
+  if (
+    !req.body.nome ||
+    typeof req.body.nome == undefined ||
+    req.body.nome == null
+  ) {
+    erros.push({ texto: "Nome Invalido" });
+  }
+
+  if (
+    !req.body.slug ||
+    typeof req.body.slug == undefined ||
+    req.body.slug == null
+  ) {
+    erros.push({ texto: "Slug Invalido" });
+  }
+
+  if (req.body.nome.length < 2) {
+    erros.push({ texto: "Nome da categoria muito pequeno" });
+  }
+
+  if (erros.length > 0) {
+    res.render("admin/addcategorias", { erros: erros });
+  } else {
+    const novaCategoria = {
+      nome: req.body.nome,
+      slug: req.body.slug,
+    };
+
+    new Categoria(novaCategoria)
+      .save()
+      .then(() => {
+        req.flash("success_msg", "Categoria criada com sucesso!");
+        res.redirect("/admin/categorias");
+      })
+      .catch((err) => {
+        req.flash(
+          "error_msg",
+          "houve um erro ao salvar a categoria, tente novamente"
+        );
+        res.redirect("/admin");
+      });
+  }
+});
+
+router.get("/categorias/edit/:id", (req, res) => {
+  Categoria.findOne({ _id: req.params.id })
+    .lean()
+    .then((categoria) => {
+      res.render("admin/editcategorias", { categoria: categoria });
     })
     .catch((err) => {
-      console.log("Erro ao salvar categoria!" + err);
+      req.flash("error_msg", "Categoria não encontrada");
+      res.redirect("/admin/categorias");
+    });
+});
+
+router.post("/categorias/edit", (req, res) => {
+  Categoria.findOne({ _id: req.body.id })
+    .then((categoria) => {
+      var erros = [];
+
+      if (
+        !req.body.nome ||
+        typeof req.body.nome == undefined ||
+        req.body.nome == null
+      ) {
+        erros.push({ texto: "Nome Invalido" });
+      }
+
+      if (
+        !req.body.slug ||
+        typeof req.body.slug == undefined ||
+        req.body.slug == null
+      ) {
+        erros.push({ texto: "Slug Invalido" });
+      }
+
+      if (req.body.nome.length < 2) {
+        erros.push({ texto: "Nome da categoria muito pequeno" });
+      }
+      if (erros.length > 0) {
+        console.log("Erros encontrados:", erros);
+        console.log("Dados do formulário:", req.body);
+
+        return res.render("admin/editcategorias", {
+          erros: erros,
+          categoria: {
+            _id: req.body.id,
+            nome: req.body.nome,
+            slug: req.body.slug,
+          },
+        });
+      }
+
+      categoria.nome = req.body.nome;
+      categoria.slug = req.body.slug;
+
+      categoria
+        .save()
+        .then(() => {
+          req.flash("success_msg", "Categoria editada com sucesso!");
+          res.redirect("/admin/categorias");
+        })
+        .catch((err) => {
+          req.flash("error_msg", "Erro ao editar a categoria!");
+          res.redirect("/admin/categorias");
+        });
+    })
+    .catch((err) => {
+      req.flash("error.msg", "Houve um erro ao editar a categoria.");
+      res.redirect("/admin/categorias");
+    });
+});
+
+router.post("/categorias/deletar", (req, res) => {
+  Categoria.deleteOne({ _id: req.body.id })
+    .then((categoria) => {
+      req.flash("success_msg", "Categoria deletada com sucesso!");
+      res.redirect("/admin/categorias");
+    })
+    .catch((err) => {
+      req.flash("error_msg", "Houve um erro ao deletar a categoria!");
+      res.redirect("/admin/categorias");
     });
 });
 
